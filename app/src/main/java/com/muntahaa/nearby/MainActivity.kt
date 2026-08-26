c2de1981deab86900ebc8b9cebd85f9aaafc9760
Pasha-007 <m.muntahaa@gmail.com>
2026-08-26 14:22:03 +0600

    Initial project setup

---
+++ app/src/main/java/com/muntahaa/nearby/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.muntahaa.nearby
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.muntahaa.nearby.ui.theme.NearbyTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NearbyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Pasha",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NearbyTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/muntahaa/nearby/NearbyApplication.kt
@@ -0,0 +1,7 @@
+package com.muntahaa.nearby
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NearbyApplication : Application()
